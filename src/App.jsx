d7473f6a59b13205c0b63beafdbb50c6d921a3fc
Karthika Anil <karthikaa10z@gmail.com>
2026-02-11 19:34:11 +0530

    Added react custom hook to organize API calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,11 @@
 import styles from './App.module.css';
-import { useEffect, useState } from 'react';
 import { ToDoForm } from './components/ToDoForm/ToDoForm';
 import { ToDoList } from './components/ToDoList/ToDoList';
 import { ToDoFilters } from './components/ToDoFilters/ToDoFilters';
-import { api } from './api';
+import { useTodos } from './hooks/todo';
 
 function App() {
-  const [todos, setToDos] = useState([]);
-  const [filters, setFilters] = useState({});
-
-  async function fetchTodos() {
-    try {
-      const data = await api.todos.getAll(filters);
-      setToDos(data);
-    } catch(error) {
-      console.log("Failed to get todo's. Please try again later.");
-    }
-  }
-
-  useEffect(() => {
-    fetchTodos();
-  }, [filters])
-
-  async function handleCreate(newTodo) {
-    try {
-      await api.todos.create(newTodo);
-      await fetchTodos();
-    } catch(error) {
-      console.log("Failed to create todo. Please try again later.");
-    }
-  }
-
-  async function handleUpdate(id, newTodo) {
-    try {
-      await api.todos.update(id, newTodo);
-      await fetchTodos();
-    } catch(error) {
-      console.log("Failed to update todo. Please try again later.");
-    }
-  }
-
-  async function handleDelete(id) {
-    try {
-      await api.todos.delete(id);
-      await fetchTodos();
-    } catch(error) {
-      console.log("Failed to delete todo. Please try again later.");
-    }
-  }
-
+  const todos = useTodos();
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
@@ -57,9 +14,9 @@ function App() {
       </header>
 
       <div className={styles.AppContainer}>
-        <ToDoForm onCreate={handleCreate} />
-        <ToDoFilters onFilters={setFilters} />
-        <ToDoList todos={todos} onUpdate={handleUpdate} onDelete={handleDelete}/>
+        <ToDoForm onCreate={todos.create} />
+        <ToDoFilters onFilters={todos.filter} />
+        <ToDoList todos={todos.data} onUpdate={todos.update} onDelete={todos.delete}/>
       </div>
     </div>
   )
